d50a64ff58d2a882e21ba9b2feeaaddb63456c82
Christian Visintin <christian.visintin@veeso.dev>
2026-08-24 12:56:53 +0200

    fix(android): add translation support for Cantonese using zh-CN. Works fine as a workaround until Android supports Cantonese translations natively

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.veeso.biangbianghanzi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 51
-        versionName = "0.5.1"
+        versionCode = 52
+        versionName = "0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
